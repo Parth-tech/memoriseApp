73073a82bb01ba5645bb66b4cc9cd822986faa18
Parth-tech <pjangid@usc.edu>
2023-11-12 13:02:20 -0800

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Parth Rasu Jangid on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
